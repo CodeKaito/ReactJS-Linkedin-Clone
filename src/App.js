1f0ab1a00d73a42360232c9c8271e0c9f0c86684
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-04-05 13:08:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
-import './App.css';
 import { Header } from './components';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        App
-      </h1>
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './header.css'
 
+import SearchIcon from '@material-ui/icons/Search';
+
 function Header() {
   return (
     <div className='header'>
       <div className='header__left'>
+        <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="" />
 
+        <div className="header__search">
+            <SearchIcon />
+            <input type="text" />
+        </div>
       </div>
       <div className='header__right'>
 
